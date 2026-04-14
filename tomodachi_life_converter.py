8c73c9b2ba671e241282c26a53e988c50e200482
swablueme <swablueme@gmail.com>
2026-04-14 23:04:30 +1000

    readd asserts

--- tomodachi_life_converter.py
+++ tomodachi_life_converter.py
@@ -92,11 +92,13 @@ def convert_png_to_canvas(canvas_path):
         canvas = set_image_gamma(canvas, ENCODING_GAMMA)
 
     """
-    Clothing etc are of a different canvas size!
-    so let's comment this assert out
-        # assert canvas.size == (
-    #     HEIGHT_OF_IMAGE, WIDTH_OF_IMAGE), "Canvas must be 256 x 256 in dimensions"
+    Clothing = 256 x 256
+    Facepaint = 256 x 256
+    Food = 256 x 256
+
     """
+    assert canvas.size == (
+        HEIGHT_OF_IMAGE, WIDTH_OF_IMAGE), "Canvas must be 256 x 256 in dimensions"
 
     canvas = canvas.convert(IMAGE_MODE).tobytes('raw')
     save_canvas(canvas, canvas_path)
